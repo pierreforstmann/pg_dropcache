b94af6537a3436fc05ec2ae949b04e0d28c66d53
Pierre Forstmann <pierre.forstmann@gmail.com>
2026-04-03 17:59:09 +0200

    adapted for PG 14, 15, 16 and 17

--- pg_dropcache--0.2.sql
+++ pg_dropcache--0.2.sql
@@ -1,11 +1,11 @@
 \echo Use "CREATE EXTENSION pg_dropcache" to load this file. \quit
 
-CREATE OR REPLACE FUNCTION pg_dropcache()
+CREATE OR REPLACE FUNCTION pg_drop_cache()
 RETURNS VOID
-AS 'pg_dropcache', 'pg_dropcache'
+AS 'pg_dropcache', 'pg_drop_cache'
 LANGUAGE C;
 
-CREATE OR REPLACE FUNCTION pg_drop_rel_cache(relation regclass, fork text default null)
+CREATE OR REPLACE FUNCTION pg_drop_rel_cache(relation regclass)
 RETURNS VOID
 AS 'pg_dropcache', 'pg_drop_rel_cache'
 LANGUAGE C;
--- pg_dropcache.c
+++ pg_dropcache.c
@@ -4,11 +4,11 @@
  *  pg_dropcache.c
  *
  *
- *	Portions Copyright (c) 2018, Ildar Musin
+ *  Portions Copyright (c) 2018, Ildar Musin
  *  https://github.com/zilder/pg_dropcache
  *
  *   
- *  Portions Copyright (c) 2025, Pierre Forstmann
+ *  Portions Copyright (c) 2026, Pierre Forstmann
  *
  *-------------------------------------------------------------------------
  */
@@ -21,14 +21,17 @@
 #include "miscadmin.h"
 #include "catalog/pg_class.h"
 #include "storage/smgr.h"
+#include "common/relpath.h"
+#include "utils/rel.h"
+#include "access/relation.h"
 
 PG_MODULE_MAGIC;
 
-PG_FUNCTION_INFO_V1(pg_dropcache);
+PG_FUNCTION_INFO_V1(pg_drop_cache);
 PG_FUNCTION_INFO_V1(pg_drop_rel_cache);
 
 Datum
-pg_dropcache(PG_FUNCTION_ARGS)
+pg_drop_cache(PG_FUNCTION_ARGS)
 {
 	DropDatabaseBuffers(MyDatabaseId);
 	PG_RETURN_VOID();
@@ -38,46 +41,34 @@ Datum
 pg_drop_rel_cache(PG_FUNCTION_ARGS)
 {
 	Oid			relid = PG_GETARG_OID(0);
-	HeapTuple	tp;
-	RelFileLocatorBackend	rnode;
-	BlockNumber	nblocks = 0;
-    ForkNumber  forks[MAX_FORKNUM];
-	SMgrRelation srel;
-	int         nforks = 0;
-
-	tp = SearchSysCache1(RELOID, ObjectIdGetDatum(relid));
-	if (HeapTupleIsValid(tp))
-	{
-		Form_pg_class reltup = (Form_pg_class) GETSTRUCT(tp);
-	
-		rnode.locator.relNumber = reltup->relfilenode;
-		rnode.locator.spcOid = (reltup->reltablespace == InvalidOid) ?
-			MyDatabaseTableSpace :
-			reltup->reltablespace;
-		rnode.locator.dbOid = MyDatabaseId;
-		rnode.backend = InvalidBackendId;
-
-		ReleaseSysCache(tp);
-	}
-	else
-	{
-		elog(ERROR, "Unable to get cache for relation %u", relid);
-		PG_RETURN_VOID();
-	}
+	BlockNumber		nblocks = 0;
+	ForkNumber  		forks[MAX_FORKNUM];
+	SMgrRelation 		srel;
+	Relation 		rel;
+	int         		nforks = 0;
 
 	/*
 	 * see RelationTruncate (storage.c) 
 	 *     -> smgrtruncate (smgr.c)
 	 *        -> DropRelationBuffers (bufmgr.c)
 	 */
-	forks[nforks] = MAIN_FORKNUM;
-	nforks = 1;
-	/*
-	 * TO DO: take into account FSM and visibility map
-	 */
-	srel  = smgropen(rnode.locator, InvalidBackendId);
-	DropRelationBuffers(srel, forks, nforks, &nblocks);
 
+	for (ForkNumber fork = 0; fork < MAX_FORKNUM; fork++)
+	{
+		forks[0] = fork;
+		nforks = 1;
+
+		rel = relation_open(relid, AccessShareLock);
+#if PG_VERSION_NUM >= 160000
+		srel = smgropen(rel->rd_locator, rel->rd_backend);
+		DropRelationBuffers(srel, forks, nforks, &nblocks);
+#else
+		srel  = smgropen(rel->rd_node, InvalidBackendId);
+		DropRelFileNodeBuffers(srel, forks, nforks, &nblocks);
+#endif
+		smgrclose(srel);
+		relation_close(rel, AccessShareLock);
+	}
 
 	PG_RETURN_VOID();
 }
